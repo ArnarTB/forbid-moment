43da9a440a77814f527a21c4bf96e89afb797bfc
Arnar <104862164+ArnarHub@users.noreply.github.com>
2024-04-07 15:25:31 +0000

    small change

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const fs = require("fs");
-const path = require("path");
-
-const ruleFiles = fs
-  .readdirSync(__dirname)
-  .filter((file) => file !== "index.js" && !file.endsWith("test.js"));
-
-const rules = Object.fromEntries(
-  ruleFiles.map((file) => [path.basename(file, ".js"), require("./" + file)])
-);
-
-module.exports = { rules: {'noMoment': require('./noMoment')} };
+module.exports = {
+  rules: {
+    'forbid-moment': require('./noMoment')
+  }
+};
