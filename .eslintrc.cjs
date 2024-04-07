5288c072c91fbe220c59fb9a28bb989be88cd119
Arnar <104862164+ArnarHub@users.noreply.github.com>
2024-04-07 14:26:00 +0000

    json config

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true
+    },
+    extends: ["eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [
+                ".eslintrc.{js,cjs}"
+            ],
+            parserOptions: {
+                sourceType: "script"
+            }
+        }
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["@typescript-eslint", "rule"],
+    rules: {
+        "rule/noMoment": "error",
+    }
+  };
